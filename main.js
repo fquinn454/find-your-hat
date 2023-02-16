2dd5bfc0afcfb00d36fcdba2fd9695e4b72a6dea
fquinn454 <57137659+fquinn454@users.noreply.github.com>
2023-02-16 15:16:22 +0000

    changed field to maze with random starting point

--- main.js
+++ main.js
@@ -4,58 +4,97 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
-let playing = true;
+playing = true;
+const up = [-1, 0];
+const down = [1, 0];
+const right = [0, 1];
+const left = [0, -1];
 
-class Field {
-  constructor(field){
-    this.field = field;
-    this.playerPosition = [0,0];
-  }
+// Generates a random point in the nested array
+// Used to select random starting point, end point and holes
+function randomNumber(mazeHeight, mazeWidth){
+    let randomY = Math.floor(Math.random()* mazeHeight);
+    let randomX = Math.floor(Math.random()* mazeWidth);
+    return [randomY, randomX];
+}
 
-// random number generator for position of items in field
-static randomNumber(num_Y, num_X){
-  let random_Y = Math.floor(Math.random()*num_Y);
-  let random_X = Math.floor(Math.random()*num_X);
-  return [random_Y, random_X];
+// used to check if a randomly selected point in the nested array is already a hole
+function positionInArray(position, array){
+    for(let i = 0; i < array.length; i++){
+        if(array[i][0] === position[0] && array[i][1] === position[1]){
+            return true;
+        }
+    }
+    return false;
 }
-// generate fields of different sizes with certain percentage of holes
-  static generateField(num_y, num_x, percentage){
-    const field = [];
-    for(let i = 0; i < num_y; i++){
-      field.push([]);
-      for(let j = 0; j < num_x; j++){
-        field[i][j] = fieldCharacter;
-      }
+/* Create a class Maze.
+    - Uses nested arrays to represent a Maze
+    - Print the maze
+*/
+class Maze{
+    constructor(mazeHeight, mazeWidth, percentage){
+        this.maze = [];
+        this.holes = [];
+        this.mazeHeight = mazeHeight;
+        this.mazeWidth = mazeWidth;
+
+        // Create an nested array of size mazeHeight and mazeWidth
+        for(let i = 0; i < mazeHeight; i++){
+            this.maze.push([]);
+            for(let j=0; j < mazeWidth; j++){
+                // Set all the characters in the maze to fieldCharacters initially
+                this.maze[i][j] = fieldCharacter;
+            }
+        }
+
+        // Use random number to select the holes in the grid.
+        // The number of holes are give by the percentage
+        for(let i = 0; i < Math.ceil(mazeHeight * mazeWidth * (percentage/100)); i++){
+            let nextHole;
+            do{
+                nextHole = (randomNumber(mazeHeight, mazeWidth));
+            }
+            // If randomly chosen place is already a hole, choose again
+            while(positionInArray(nextHole, this.holes));
+            this.holes.push(nextHole);
+        }
+        // Add the hole symbol to the maze for the previously selected holes
+        for(let i = 0; i < this.holes.length; i++){
+            this.maze[this.holes[i][0]][this.holes[i][1]] = hole;
+        }
+
+        // Chooses a starting point, must not be a hole
+        do{
+            this.startingPoint = randomNumber(mazeHeight, mazeWidth);
+        }
+        while(positionInArray(this.startingPoint, this.holes));
+        this.maze[this.startingPoint[0]][this.startingPoint[1]] = pathCharacter;
+        
+        // Chooses an ending point, must not be a hole or the starting point
+        do{
+            this.endPoint = randomNumber(mazeHeight, mazeWidth);
+        }
+        while(positionInArray(this.endPoint, this.holes) || this.endPoint === this.startingPoint);
+        this.maze[this.endPoint[0]][this.endPoint[1]] = hat;
+
+        this.playerPosition = [this.startingPoint[0], this.startingPoint[1]];
     }
-  // add start position
-  field[0][0] = pathCharacter;
-  // add one hat
-  let hatPosition = [0,0];
-  while(hatPosition[0] === 0 && hatPosition[1] === 0){
-    hatPosition = this.randomNumber(num_y, num_x);
-  }
-  field[hatPosition[0]][hatPosition[1]] = hat;
-  
-  
-  // add chosen % holes to field
-  const fieldSize = num_y * num_x;
-  for(let k = 0; k < Math.ceil(fieldSize * (percentage/100)); k++){
-    let holePosition = this.randomNumber(num_y, num_x);
-    if(holePosition[0] !== 0 && holePosition[1] !== 0){
-      if(field[holePosition[0]][holePosition[1]] !== hat ){
-        field[holePosition[0]][holePosition[1]] = hole;
-      }
+
+    // Prints each array in the nested array as a line in the maze
+    print(){
+        for(let i = 0; i < this.maze.length; i++){
+          let outputString = "";
+          for(let j = 0; j < this.maze[i].length; j++){
+            outputString += this.maze[i][j];
+          }
+          console.log(outputString);
+        }
     }
-    else{
-      k--;
-    }
-  }
-  return field;
-}
+    
 
 // Check if the player is still on the field
   checkValidPosition(num_y, num_x){
-    if(num_y >= this.field.length || num_x >= this.field[0].length || num_x < 0 || num_y < 0){
+    if(num_y >= this.maze.length || num_x >= this.maze[0].length || num_x < 0 || num_y < 0){
       term.red("You are out of bounds. Stay in the field!");
       playing = false;
       return playing;
@@ -87,17 +126,18 @@ static randomNumber(num_Y, num_X){
 
 // When the player is on a position change the character to show visited
     set changeFieldCharacter(character){
-        this.field[this.playerPosition_Y][this.playerPosition_X] = character;
+        this.maze[this.playerPosition_Y][this.playerPosition_X] = character;
     }
 
   // Print the Field
-print(){
-    for(let i = 0; i < this.field.length; i++){
+
+  print(){
+    for(let i = 0; i < this.maze.length; i++){
       let outputString = "";
-      for(let j = 0; j < this.field[i].length; j++){
-        outputString += this.field[i][j];
+      for(let j = 0; j < this.maze[i].length; j++){
+        outputString += this.maze[i][j];
       }
-      for(let k = 0; k < this.field[0].length; k++){
+      for(let k = 0; k < this.maze[0].length; k++){
         if(outputString.charAt(k) === hat){
           term.brightGreen(outputString.charAt(k));
         }
@@ -143,11 +183,11 @@ print(){
   }
   // Check if player win or lose
   checkPlayerStatus(){
-    if (this.field[this.playerPosition_Y][this.playerPosition_X] === hat){
+    if (this.maze[this.playerPosition_Y][this.playerPosition_X] === hat){
         console.log("You win! Found your hat");
         playing = false;
     }
-    else if (this.field[this.playerPosition_Y][this.playerPosition_X] === hole){
+    else if (this.maze[this.playerPosition_Y][this.playerPosition_X] === hole){
         console.log("You fell in a hole!");
         playing = false;
         }
@@ -158,18 +198,18 @@ print(){
   }
 }
 
-function playGame(field){
+function playGame(maze){
     while(playing){
-      field.print();
+      maze.print();
       let direction = prompt(term.blue("Which way? "));
         switch(direction){
-            case "r" : field.movePlayerRight();
+            case "r" : maze.movePlayerRight();
             break;
-            case "l" : field.movePlayerLeft();
+            case "l" : maze.movePlayerLeft();
             break;
-            case "u" : field.movePlayerUp();
+            case "u" : maze.movePlayerUp();
             break;
-            case "d" : field.movePlayerDown();
+            case "d" : maze.movePlayerDown();
             break;
             default : term.red("Which way? Right(r), Left(l), Up(u), or Down(d)");
             console.log();
@@ -177,6 +217,6 @@ function playGame(field){
     }
 }
 
-const myField = new Field(Field.generateField(4, 5, 40));
+const myField = new Maze(4, 5, 40);
 playGame(myField);
 
