6d8d2970311795383f6ff857de2e9a43b246761c
fquinn454 <57137659+fquinn454@users.noreply.github.com>
2023-02-18 10:54:21 +0000

    Added maze solver
    Maze solver checks that the maze can be solved before starting the game

--- main.js
+++ main.js
@@ -5,28 +5,27 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 playing = true;
-const up = [-1, 0];
-const down = [1, 0];
-const right = [0, 1];
-const left = [0, -1];
 
-// Generates a random point in the nested array
-// Used to select random starting point, end point and holes
+/* Generates a random point in the nested array
+   Used to select random starting point, end point and holes */
 function randomNumber(mazeHeight, mazeWidth){
     let randomY = Math.floor(Math.random()* mazeHeight);
     let randomX = Math.floor(Math.random()* mazeWidth);
     return [randomY, randomX];
 }
 
-// used to check if a randomly selected point in the nested array is already a hole
-function positionInArray(position, array){
-    for(let i = 0; i < array.length; i++){
-        if(array[i][0] === position[0] && array[i][1] === position[1]){
-            return true;
-        }
+// Returns true - a square on the maze is a hole 
+function isAHole(maze, square){
+  for(let hole in maze.holes){
+    if(hole[0] === square[0] && hole[1] === square[1]){
+      return true;
     }
-    return false;
+    else{
+      return false;
+    }
+  }
 }
+
 /* Create a class Maze.
     - Uses nested arrays to represent a Maze
     - Print the maze
@@ -55,7 +54,7 @@ class Maze{
                 nextHole = (randomNumber(mazeHeight, mazeWidth));
             }
             // If randomly chosen place is already a hole, choose again
-            while(positionInArray(nextHole, this.holes));
+            while(isAHole(this.maze, nextHole));
             this.holes.push(nextHole);
         }
         // Add the hole symbol to the maze for the previously selected holes
@@ -67,14 +66,14 @@ class Maze{
         do{
             this.startingPoint = randomNumber(mazeHeight, mazeWidth);
         }
-        while(positionInArray(this.startingPoint, this.holes));
+        while(isAHole(this.maze, this.startingPoint));
         this.maze[this.startingPoint[0]][this.startingPoint[1]] = pathCharacter;
         
         // Chooses an ending point, must not be a hole or the starting point
         do{
             this.endPoint = randomNumber(mazeHeight, mazeWidth);
         }
-        while(positionInArray(this.endPoint, this.holes) || this.endPoint === this.startingPoint);
+        while(isAHole(this.maze, this.endPoint));
         this.maze[this.endPoint[0]][this.endPoint[1]] = hat;
 
         this.playerPosition = [this.startingPoint[0], this.startingPoint[1]];
@@ -197,6 +196,43 @@ class Maze{
     }   
   }
 }
+// Check if maze can be solved
+function solveMaze(maze){
+  const stack = [[maze.startingPoint[0], maze.startingPoint[1]]];
+  const visited = [];
+  while (stack.length > 0) {
+      let current = stack.pop();
+      // Check if we've reached the end point
+      if (current[0] === maze.endPoint[0] && current[1] === maze.endPoint[1]) {
+        return true;
+      }
+      visited.push(current);
+
+      // Check all adjacent next squares
+      for (const [dx, dy] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+        const nextX = current[0] + dx;
+        const nextY = current[1] + dy;
+        
+        // Check if the next square is within the maze
+        if (nextX < 0 || nextX >= maze.mazeWidth || nextY < 0 || nextY >= maze.mazeHeight) {
+          // If not in maze terminate current execution of loop and start next iteration
+          continue;
+        }
+        
+        // Check if the next square is a hole or has already been visited
+        if (maze.holes.some(([x, y]) => x === nextX && y === nextY)|| visited.some(([x, y]) => x === nextX && y === nextY)){
+          // If a hole or already visited terminate current execution of loop and start next iteration
+          continue;
+        }
+        
+        // Add the next square to the stack
+        stack.push([nextX, nextY]);
+      }
+  }
+    // If we've reached this point, the maze is unsolvable
+    return false;
+}
+
 
 function playGame(maze){
     while(playing){
@@ -217,6 +253,13 @@ function playGame(maze){
     }
 }
 
-const myField = new Maze(4, 5, 40);
-playGame(myField);
+let myMaze = new Maze(4, 8, 50);
+do {
+  myMaze = new Maze(4, 8, 50);
+}
+while(!solveMaze(myMaze));
+
+playGame(myMaze);
+
+
 
